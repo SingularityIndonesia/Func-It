710a2adeb870dd77e1b056783b304e27935e1f63
stefanus <stefanus.ayudha@gmail.com>
2023-09-12 14:22:08 +0700

    update test example

--- src/commonTest/kotlin/com/singularity_indonesia/func_it/CompositionTest.kt
+++ src/commonTest/kotlin/com/singularity_indonesia/func_it/CompositionTest.kt
@@ -1,7 +1,8 @@
 package com.singularity_indonesia.func_it
 
+import arrow.core.Option
+import arrow.core.some
 import kotlin.test.Test
-import kotlin.test.assertEquals
 
 /**
  * Created by: stefanus
@@ -9,29 +10,66 @@ import kotlin.test.assertEquals
  * Design by: stefanus.ayudha@gmail.com
  */
 class CompositionTest {
-    fun f(x: Double): Double {
-        // Define the implementation of f
-        return x + 1
-    }
 
-    fun g(x: Double): Double {
-        // Define the implementation of g
-        return x * 2
-    }
+    @JvmInline
+    value class Token(val value: String)
 
-    fun h(x: Double): Double {
-        // Define the implementation of h
-        return x - 3
+    @JvmInline
+    value class UserID(val value: String)
+
+    @JvmInline
+    value class UserName(val value: String)
+
+    @JvmInline
+    value class Email(val value: String)
+
+    data class UserProfile(
+        val id: UserID,
+        val name: UserName,
+        val email: Email
+    )
+
+    data class UserProfileDisplayable(
+        val userProfile: UserProfile,
+        val enabled: Boolean,
+        val selected: Boolean
+    )
+
+    fun userIDOf(token: Token) =
+        UserID(
+            "Some ID"
+        ).some()
+
+
+    fun userProfileOf(userID: Option<UserID>) =
+        userID.map {
+            UserProfile(
+                id = it,
+                name = UserName("Some Name"),
+                email = Email("Some Email")
+            )
+        }
+
+    fun displayableUserProfileOf(userProfile: Option<UserProfile>) =
+        userProfile.map {
+            UserProfileDisplayable(
+                userProfile = it,
+                enabled = true,
+                selected = false
+            )
+        }
+
+    fun getUserProfileDisplay(token: Token) = ::displayableUserProfileOf o ::userProfileOf o ::userIDOf o token
+
+    @Test
+    fun testComposeUserDiplayableFromToken() {
+        val userToken: Token = Token("some Token")
+
+        val displayableUserProfile = ::getUserProfileDisplay o userToken
+
+        assert(displayableUserProfile.isSome())
     }
 
     @Test
-    fun testComposition() {
-        val x = 10.0 // Replace with your desired input value
-
-        // Compose the functions and apply them to x
-        val result = ::f o ::g o ::h o x
-
-        // result ((10 - 3) * 2) + 1
-        assertEquals(15.0, result)
-    }
+    fun testImperative() {}
 }
